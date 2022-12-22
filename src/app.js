e2f2d8a0d317b6ba763184a64e1404cf1325fb0e
Mariano Cuartero <cuarteroma@gmail.com>
2022-12-22 16:04:12 -0300

    Añadido WebSocket y HandleBars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,39 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import __dirname from './utils.js';
+
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
 const app = express();
 
-app.use(express.json())
+const httpServer = app.listen(8080, ()=>{
+    console.log("Server listening on port 8080...");
+});
 
+const socketServer = new Server(httpServer)
+
+app.use(express.json())
+app.use(express.static(__dirname + '/public/'))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars')
+
+//Utilizamos este Middleware genérico para enviar la instancia del servidor de Socket.io a las routes
+app.use((req,res,next)=>{
+    req.io = socketServer
+    next()
+})
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
-app.listen(8080, ()=>{
-    console.log("Server listening on port 8080...");
-});
+socketServer.on('connection', socket =>{
+    console.log(socket.id);
+    socket.on('msg_front', message => console.log(message));
+    socket.emit('msg_back',"Conectado al servicio, Bienvenido desde el Back")
+    /* socket.emit('msg_individual', 'Este msj solo lo recibe el socket')
+    socket.broadcast.emit('msg_resto','Este msj lo recibe todos menos el socket actual')
+    socketServer.emit('msg_all','Mensaje a todos') */
+})
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -19,6 +19,8 @@ router.get('/products', async (req, res) => {
         }
         res.send({prodLimit})
     }
+    req.io.emit('updatedProducts', products);
+
 })
 
 router.get('/products/:pid', async (req, res) => {
@@ -30,20 +32,43 @@ router.get('/products/:pid', async (req, res) => {
 router.post('/', async (req, res) => {
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
     const addProduct = await manager.addProduct(title, description, price, code, stock, category, status, thumbnails)
+    req.io.emit('updatedProducts', await manager.getProducts());
     res.send(addProduct)
+
 })
 
 router.put('/:pid', async (req, res) => {
     const id = parseInt(req.params.pid)
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
     const updateProduct = await manager.updateProductById(id, title, description, price, code, stock, category, status, thumbnails)
+    req.io.emit('updatedProducts', await manager.getProducts());
     res.send(updateProduct)
 })
 
 router.delete('/:pid', async (req, res) => {
     const id = parseInt(req.params.pid)
     const deleteProduct =  await manager.deleteProductById(id)
+    req.io.emit('updatedProducts', await manager.getProducts());
     res.send(deleteProduct)
+})
+
+router.get('/home', async (req, res) =>{
+    const products = await manager.getProducts()
+    res.render('home',
+    {
+        title: "Lista de Productos",
+        products: products
+    })
+})
+
+router.get('/realtimeproducts', async (req, res) =>{
+    const products = await manager.getProducts()
+    res.render('realTimeProducts',
+    {
+        title: "Lista de Productos",
+        products: products
+    })
+
 })
 
 export default router;
